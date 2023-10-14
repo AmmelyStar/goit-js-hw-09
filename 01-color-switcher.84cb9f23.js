c8270ecacaae50a09c1ecf71d2980bf6189499ab
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2023-10-14 09:16:22 +0000

    Deploying to gh-pages from @ AmmelyStar/goit-js-hw-09@f21ff6f4c9ff134b96bb7efe88736ccf93dff1c5 🚀

---
+++ 01-color-switcher.84cb9f23.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e;t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.84cb9f23.js.map
